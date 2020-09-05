0a1ca329bb4bb20911fa38008103f9b282d5d5d3
rodeagle <57577502+rodeagle@users.noreply.github.com>
2020-09-05 00:53:16 -0500

    Upload Base Project
    Base project uploaded

---
+++ data.js
@@ -0,0 +1,110 @@
+var data = {
+    "products": [
+        {
+            id : 1,
+            "description" : "",
+            "price" : "",
+            "discount-price" : "",
+            "img-path" : ""
+        },
+        // beverages
+        {
+            id : 100,
+            "description" : "",
+            "price" : "",
+            "discount-price" : "",
+            "img-path" : ""
+        },
+        // appetizers
+        {
+            id : 200,
+            title : "Guacomole Dip",
+            "description" : "Our finest avocado selection made to the best combination",
+            "price" : "3.99",
+            "discount-price" : "",
+            "img-path" : "images/appetizer/guacamole.jpg"
+        },
+        {
+            id : 201,
+            title : "Guacomole Dip",
+            "description" : "Our finest avocado selection made to the best combination",
+            "price" : "3.99",
+            "discount-price" : "",
+            "img-path" : "images/appetizer/guacamole.jpg"
+        },
+        {
+            id : 202,
+            title : "Guacomole Dip",
+            "description" : "Our finest avocado selection made to the best combination",
+            "price" : "3.99",
+            "discount-price" : "",
+            "img-path" : "images/appetizer/guacamole.jpg"
+        },
+        {
+            id : 203,
+            title : "Guacomole Dip",
+            "description" : "Our finest avocado selection made to the best combination",
+            "price" : "3.99",
+            "discount-price" : "",
+            "img-path" : "images/appetizer/guacamole.jpg"
+        },
+        {
+            id : 204,
+            title : "Guacomole Dip",
+            "description" : "Our finest avocado selection made to the best combination",
+            "price" : "3.99",
+            "discount-price" : "",
+            "img-path" : "images/appetizer/guacamole.jpg"
+        },
+        {
+            id : 205,
+            title : "Guacomole Dip",
+            "description" : "Our finest avocado selection made to the best combination",
+            "price" : "3.99",
+            "discount-price" : "",
+            "img-path" : "images/appetizer/guacamole.jpg"
+        },        
+        {
+            id : 206,
+            title : "Guacomole Dip",
+            "description" : "Our finest avocado selection made to the best combination",
+            "price" : "3.99",
+            "discount-price" : "",
+            "img-path" : "images/appetizer/guacamole.jpg"
+        },
+        {
+            id : 207,
+            title : "Guacomole Dip",
+            "description" : "Our finest avocado selection made to the best combination",
+            "price" : "3.99",
+            "discount-price" : "",
+            "img-path" : "images/appetizer/guacamole.jpg"
+        },
+    ],
+    "locations": {
+        "paris" : {
+            "location" : "PARIS",
+            "street" : "E Jasper 508",
+            "city" : "Paris",
+            "zipcode" : "61944",
+            "state" : "IL",
+            "phone" : "70810320123",
+            hours : 'SUN - THR 10:00AM - 9:00PM <br> FRI - SAT 10:00AM - 10:00PM',
+            maindishes : [1],
+            beverages : [100],
+            appetizers : [200,201,202,203,204,205,206,207]
+        },
+        "paris2" : {
+            "location" : "PARIS",
+            "street" : "E Jasper 508",
+            "city" : "Paris",
+            "zipcode" : "61944",
+            "state" : "IL",
+            "phone" : "70810320123",
+            hours : 'SUN - THR 10:00AM - 9:00PM <br> FRI - SAT 10:00AM - 10:00PM',
+            maindishes : [1],
+            beverages : [100],
+            appetizers : [200,201,202,203,204,205,206,207]
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,141 @@
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+function JsonObjectToArray(obj){
+    return Object.keys(obj)
+    .map(function(key) {
+        return obj[key];
+    });
+}
+
+window.addEventListener('orientationchange', function ()
+{
+    if (window.innerHeight > window.innerWidth)
+    {
+        document.getElementsByTagName('body')[0].style.transform = "rotate(90deg)";
+    }
+});
+
+
+var local = getUrlParameter('location');
+
+local = !local ? 'paris' : local; 
+
+console.log(data);
+
+var _location = data.locations[local];
+
+console.log(_location);
+
+class Address{
+    template(){
+        return `
+            <div class="text-center text-light font-weight-bold">
+                {{street}}<br>
+                {{city}} {{state}} {{zipcode}} <br>
+                {{phone}} <br><br>
+                {{{hours}}}
+            </div>
+        `;
+    }
+}
+
+class AddressLight {
+    template(){
+        return `
+            <div class="text-center">
+                {{street}}<br>
+                {{city}} {{state}} {{zipcode}} <br>
+                {{phone}} <br><br> 
+                {{{hours}}}
+            </div>
+        `;
+    }
+}
+
+class Locations{
+    template(){
+        return `
+            {{#each locations}}
+                <hr>
+                <div class="p-5">
+                    ${new AddressLight().template()}
+                </div>
+            {{/each}}
+        `;
+    }
+}
+
+class Products {
+    template(){
+        return `
+        {{#each items}}
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-3 product" style="position:relative">
+                <img class="" style="object-fit: fill"  width="100%" height="100%" src="{{img-path}}">
+                <div class="info-container" style="display:grid;grid-template-columns:auto;grid-template-rows:7vh 20vh 6vh; position:absolute;z-index:10;top:0;left:0;">
+                    <div style="grid-column: 1/2;">
+                        <div class="p-2 font-weight-bold">
+                            {{title}}
+                        </div>
+                    </div>
+                    <div style="grid-column:1/2;">
+                        {{description}}
+                    </div>
+                    <div style="grid-column:1/2">
+                        <div class="font-weight-bold">
+                        {{#if discount-price}}
+                            <div class="discounted-price">
+                                $ {{discount-price}} <span class="cross-out">$ {{price}}</span>
+                            <div>
+                            {{else}}
+                            <div class="normal-price">
+                                $ {{price}}
+                            </div>
+                        {{/if}}
+                    </div>
+                    </div>
+                </div>
+            </div>
+        {{/each}}
+        `;
+    }
+}
+
+class CallUsNowButton{
+    template(){
+        return `
+            <button class="btn btn-danger p-2" href="tel:{{phone}}">CALL US NOW</button>
+        `;
+    }
+}
+
+function RenderDOM(id,element,data){
+    let html = new element().template();
+    let template = Handlebars.compile(html);
+    let result = template(data);
+    document.getElementById(id).innerHTML = result;
+}
+
+function init(){
+
+    // render element to div container
+    RenderDOM('address-display',Address,_location);
+    // render call us now button
+    RenderDOM('call-us-now',CallUsNowButton,_location);
+    // render products 
+    var _items = data.products.filter(x => _location.appetizers.includes(x.id));
+    RenderDOM('appetizer-content',Products, { items : _items });
+    // render locations
+    console.log(JsonObjectToArray(data.locations));
+    console.log(new Locations().template());
+    RenderDOM('locations-display',Locations, { locations : JsonObjectToArray( data.locations) });
+
+}
+
+document.addEventListener( "DOMContentLoaded", init);
+ 
